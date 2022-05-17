098ffed332c1ba69a102de5b498c372dda6d930c
shivansh <shivansh14402@gmail.com>
2022-05-17 13:47:06 +0530

    Expense dates completed

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,10 +1,21 @@
 import React from "react";
+import "./ExpenseItem.css"
+import ExpenseDates from "./ExpenseDates";
 
+const ExpenseItem = (props) => {
 
-const ExpenseItem = () => {
+    const month = props.date.toLocaleString("en-US", {month: "long"})
+    const day = props.date.toLocaleString("en-US", {day: "2-digit"})
+    const year = props.date.getFullYear()
 
-    return(
-        <h2></h2>
+    return (
+        <div className="expense-item">
+            <ExpenseDates date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">{props.amount}</div>
+            </div>
+        </div>
     )
 }
 
