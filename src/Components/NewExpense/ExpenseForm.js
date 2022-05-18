9182af4279261ecfdc363d995995797b5b3b9427
shivansh <shivansh14402@gmail.com>
2022-05-18 09:28:55 +0530

    submit handler added

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -5,9 +5,19 @@ import { useState } from "react";
 const ExpenseForm = () => {
 
     const [title, setTitle] = useState("")
+    const [amount, setAmount] = useState(0);
+    const [date, setDate] = useState(new Date())
 
     const titleChangeHandler = (e) => {
         setTitle(e.target.value)
+    }
+
+    const amountChangeHandler = (e) => {
+        setAmount(e.target.value)
+    }
+
+    const dateChangeHandler = (e) => {
+        setDate(e.target.value)
     }
 
     return(
@@ -18,11 +28,11 @@ const ExpenseForm = () => {
             </div>
             <div className="new-expense__control">
                 <label>amount</label>
-                <input type="number" min="0.01" step="0.01" />
+                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
             </div>
             <div className="new-expense__control">
                 <label>date</label>
-                <input type="date" min="2019-01-01" max="2022-12-31"/>
+                <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
             </div>
             <div className="new-expense__actions"><button type="submit">Add Expense</button></div>
         </form>
